c7c1a95087e5c951956b4da12313eb58232a3469
realchanatong <88615326+tongcns@users.noreply.github.com>
2024-06-20 19:11:20 +0700

    [ADD] pillsManagePage, Create(components in create) to fix frequency

---
+++ careyou/lib/components/createPillsCard.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class CreatePillsCard extends StatelessWidget {
+  const CreatePillsCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return SizedBox(
+      child: Card(
+        child: Container(
+          width: screenWidth * 0.9,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: const Offset(0, 1),
+                blurRadius: 6.5,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              // Uncomment this section if you have a PillsImage widget
+              // Padding(
+              //   padding: EdgeInsets.all(screenWidth * 0.02),
+              //   child: Container(
+              //     width: screenWidth * 0.18,
+              //     height: screenWidth * 0.18,
+              //     decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(5),
+              //     ),
+              //     child: const PillsImage(),
+              //   ),
+              // ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: screenHeight * 0.01),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 5.0, left: 15.0, bottom: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildTextField(
+                          screenWidth,
+                          'Pill Name:',
+                          'Type pill name, treat for, label',
+                          context,
+                        ),
+                        SizedBox(height: screenHeight * 0.02),
+                        _buildTextField(
+                          screenWidth,
+                          'Note:',
+                          'Add more detail',
+                          context,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(
+    double screenWidth,
+    String labelText,
+    String hintText,
+    BuildContext context,
+  ) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          labelText,
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: screenWidth * 0.03,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF00916E),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            height: 25,
+            margin: EdgeInsets.symmetric(
+                horizontal: 10), // Adjust horizontal margin
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Color(0xFF00916E), // Underline color
+                  width: 1, // Underline width
+                ),
+              ),
+            ),
+            child: TextFormField(
+              style: const TextStyle(fontSize: 12),
+              decoration: InputDecoration(
+                hintText: hintText,
+                hintStyle: TextStyle(
+                  color: Color(0xFF999797),
+                  fontSize: 12,
+                ),
+                border: InputBorder.none, // Remove default border
+                contentPadding: EdgeInsets.only(left: 5.0, bottom: 18),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ careyou/lib/components/createPillsTime.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class CreatePillsTime extends StatefulWidget {
+  const CreatePillsTime({Key? key}) : super(key: key);
+
+  @override
+  _CreatePillsTimeState createState() => _CreatePillsTimeState();
+}
+
+class _CreatePillsTimeState extends State<CreatePillsTime> {
+  List<String> pillTypes = [
+    'After Meal',
+    'Before Meal',
+    'Take with meal',
+  ];
+  String? selectedPillType;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    // double screenHeight = MediaQuery.of(context).size.height;
+
+    return SizedBox(
+      child: Card(
+        child: Container(
+          width: screenWidth * 0.9,
+          height: 50,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: const Offset(0, 1),
+                blurRadius: 6.5,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10.0, bottom: 10.0, left: 10.0, right: 4.0),
+                child: Text(
+                  'Pill Time',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: screenWidth * 0.03,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF00916E),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Row(
+                    children: pillTypes.map((type) {
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              selectedPillType = type;
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                              vertical:
+                                  10, // Adjust vertical padding to accommodate the text
+                              horizontal:
+                                  10, // Adjust horizontal padding to accommodate the text
+                            ),
+                            minimumSize: const Size(
+                                20, 30), // Set a minimum size for the button
+                            backgroundColor: selectedPillType == type
+                                ? const Color(0xFFF54900)
+                                : const Color(0xFFD1D1D1),
+                          ),
+                          child: Text(
+                            type,
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              fontFamily: 'Poppins',
+                              color: selectedPillType == type
+                                  ? const Color(0xFFFFFFFF)
+                                  : const Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
